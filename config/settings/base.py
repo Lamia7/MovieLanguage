1eb6f1cade7371efdef2f4401489415e8fb34dcb
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-07-07 14:06:36 +0200

    🔧 config: Separate settings in config directory

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,30 +13,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
-
-# ENV as development by default, check .env file if production
-ENV = os.getenv("ENV", "development")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# False if ENV=prod, by default, ENV=development (True)
-DEBUG = False if ENV == "production" else True
-
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-]
 
 # Application definition
 
@@ -62,7 +47,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "movielanguage.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -80,21 +65,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "movielanguage.wsgi.application"
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("HOST"),
-        "PORT": os.getenv("PORT"),
-    }
-}
+WSGI_APPLICATION = "config.wsgi.application"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
---
+++ config/settings/local.py
@@ -0,0 +1,38 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+from .base import *  # noqa: F401, F403
+
+
+# ENV as development by default, check .env file if production
+ENV = os.getenv("ENV")
+
+ALLOWED_HOSTS = ["*"]
+
+# load_dotenv(find_dotenv(filename=".env-local")) ?
+
+SECRET_KEY = os.getenv("SECRET_KEY", default="foo-key")
+
+# ALLOWED_HOSTS = [
+#     "localhost",
+#     "127.0.0.1",
+# ]
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# False if ENV=prod, by default, ENV=development (True)
+DEBUG = False if ENV == "production" else True
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DB_NAME", default=""),
+        "USER": os.getenv("DB_USER", default=""),
+        "PASSWORD": os.getenv("DB_PASSWORD", default=""),
+        "HOST": os.getenv("HOST", default=""),
+        "PORT": os.getenv("PORT", default=""),
+    }
+}
