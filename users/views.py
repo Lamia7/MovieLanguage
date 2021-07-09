b899ab93f4afeae20645d9082a8a2b9b75366698
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-07-09 14:41:44 +0200

    ✨ feat/users: Register view + user model

--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .forms import UserRegisterForm
 
 
 # Create your views here.
@@ -11,4 +14,20 @@ def register(request):
     Returns:
         str: Template file
     """
-    return render(request, "users/register.html")
+
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(
+                request,
+                f"Votre compte a été créé avec succès.\n"
+                f"Vous pouvez vous connecter sur le compte : {username}.",
+            )
+
+            return redirect("quizz:home")
+    else:
+        form = UserRegisterForm()
+    # form as a context to be used in template
+    return render(request, "users/register.html", {"form": form})
