5821397a0d6a98c4438bc7c0aacd6df8378e393f
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-07-12 19:27:53 +0200

    ✨ feat/users_login: Add 'login' page

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -6,4 +7,9 @@ from . import views
 app_name = "users"
 urlpatterns = [
     path("register/", views.register, name="register"),
+    path(
+        "login/", auth_views.LoginView.as_view(
+            template_name="users/login.html"),
+        name="login"
+    ),
 ]
