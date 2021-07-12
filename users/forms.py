e8708168a85854b35da885b5b047683232d94687
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-07-12 19:27:53 +0200

    ✨ feat/users_login: Add 'login' page

--- users/forms.py
+++ users/forms.py
@@ -7,9 +7,18 @@ from .models import User
 class UserRegisterForm(UserCreationForm):
     """Customized form that creates users"""
 
-    email = forms.EmailField(max_length=254, help_text="Obligatoire")
+    email = forms.EmailField(max_length=255, help_text="Obligatoire")
     username = forms.CharField(max_length=60, help_text="Obligatoire")
 
     class Meta:
         model = User
         fields = ["email", "username", "password1", "password2"]
+
+
+class UserAuthenticationForm(UserCreationForm):
+    """Customized form that authenticate users"""
+    email = forms.EmailField(max_length=255)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password2']
