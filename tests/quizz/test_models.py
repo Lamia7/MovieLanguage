6589be22e0fcd840039f1b86c9dc23b5df78ca2f
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-07-18 11:29:33 +0200

    ♻️ feat/quizz_display: Refacto of tests on quizz models

--- tests/quizz/test_models.py
+++ tests/quizz/test_models.py
@@ -4,78 +4,52 @@ from quizz.models import Answer, Language, Movie, Question, Quizz
 
 class ModelsTestCase(TestCase):
     def setUp(self):
-        self.movie_data = "Captain Fantastic"
-
-    def test_language_str(self):
-        language = Language.objects.create(name="English")
-        self.assertEqual(str(language), "English")
-
-    def test_movie_str(self):
-        movie = Movie.objects.create(title="Captain Fantastic")
-        self.assertEqual(str(movie), "Captain Fantastic")
-
-    def test_quizz_str(self):
-        movie = Movie.objects.create(title="Captain Fantastic")
-        language = Language.objects.create(name="English")
-        quizz = Quizz.objects.create(
+        self.movie_obj = Movie.objects.create(title="Captain Fantastic")
+        self.language_obj = Language.objects.create(name="English")
+        self.quizz_obj = Quizz.objects.create(
             title="Quizz1",
-            movie=movie,
+            movie=self.movie_obj,
             image="",
             question_quantity=5,
             date="",
             required_score_to_pass=50,
-            language=language
+            language=self.language_obj
         )
-        self.assertEqual(str(quizz), "Quizz1")
+        self.question_obj = Question.objects.create(
+            question_content="How many kids does Ben Cash have ?",
+            quizz=self.quizz_obj
+        )
+
+    def test_language_str(self):
+        self.assertEqual(str(self.language_obj), "English")
+
+    def test_movie_str(self):
+        self.assertEqual(str(self.movie_obj), "Captain Fantastic")
+
+    def test_quizz_str(self):
+        self.assertEqual(str(self.quizz_obj), "Quizz1")
 
     def test_get_all_questions_of_quizz(self):
         pass
 
     def test_question_str(self):
-        movie = Movie.objects.create(title="Captain Fantastic")
-        language = Language.objects.create(name="English")
-        quizz = Quizz.objects.create(
-            title="Quizz1",
-            movie=movie,
-            image="",
-            question_quantity=5,
-            date="",
-            required_score_to_pass=50,
-            language=language
+        self.assertEqual(
+            str(self.question_obj),
+            "How many kids does Ben Cash have ?"
         )
-        question = Question.objects.create(
-            question_content="How many kids does Ben Cash have ?",
-            quizz=quizz
-        )
-        self.assertEqual(str(question), "How many kids does Ben Cash have ?")
 
     def test_get_all_answers_of_question(self):
         pass
 
     def test_answer_str(self):
-        movie = Movie.objects.create(title="Captain Fantastic")
-        language = Language.objects.create(name="English")
-        quizz = Quizz.objects.create(
-            title="Quizz1",
-            movie=movie,
-            image="",
-            question_quantity=5,
-            date="",
-            required_score_to_pass=50,
-            language=language
-        )
-        question = Question.objects.create(
-            question_content="How many kids does Ben Cash have ?",
-            quizz=quizz
-        )
         answer = Answer.objects.create(
             answer_content="6",
             is_correct=True,
-            question=question,
+            question=self.question_obj,
         )
         self.assertEqual(
             str(answer),
-            "QUESTION: How many kids does Ben Cash have ? - REPONSE: 6 - CORRECT: True"
+            "QUESTION: How many kids does Ben Cash have ? - REPONSE: 6 - CORRECT: True"  # noqa: E501
         )
 
     # def test_result_str(self):
