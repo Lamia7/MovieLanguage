915c5cd731669d01f6b4de313550aa320b7e3b9e
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-07-23 23:31:12 +0200

    ♻️ refacto: Convert quizz_list from FBV into CBV + template

--- quizz/views.py
+++ quizz/views.py
@@ -1,4 +1,6 @@
+from quizz.models import Quizz
 from django.shortcuts import render
+from django.views.generic import ListView
 
 
 # Create your views here.
@@ -14,13 +16,17 @@ def home(request):
     return render(request, "quizz/home.html")
 
 
-def quizz_list(request):
-    """Displays the quizz_list page
+# def quizz_list(request):
+#     """Displays the quizz_list page
 
-    Args:
-        request (obj): Django object to generate response
+#     Args:
+#         request (obj): Django object to generate response
 
-    Returns:
-        str: Template file
-    """
-    return render(request, "quizz/quizz_list.html")
+#     Returns:
+#         str: Template file
+#     """
+#     return render(request, "quizz/quizz_list.html")
+class QuizzListView(ListView):
+    """CBV that displays the quizz_list page"""
+    model = Quizz
+    template_name = "quizz/quizz_list.html"
