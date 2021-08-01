bf7cbe7835925bd169f6a5c9bb3480cad3b0214b
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-08-01 22:40:19 +0200

    ♻️ feat/result: Clean result view code

--- quizz/views.py
+++ quizz/views.py
@@ -6,11 +6,13 @@ from quizz.models import Question, Quizz
 
 class HomeView(TemplateView):
     """CBV that displays the home page"""
+
     template_name = "quizz/home.html"
 
 
 class QuizzListView(ListView):
     """CBV that displays the quizz_list page"""
+
     model = Quizz
     template_name = "quizz/quizz_list.html"
     context_object_name = "quizz_list"
@@ -26,7 +28,7 @@ class QuizzDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(QuizzDetailView, self).get_context_data(**kwargs)
-        context['questions'] = Question.objects.filter(quizz=self.kwargs['pk'])
+        context["questions"] = Question.objects.filter(quizz=self.kwargs["pk"])
         # context['quizz'] = Quizz.objects.get(pk=self.kwargs['pk'])
         return context
 
@@ -39,7 +41,7 @@ def result(request, pk):
     # 1- count the number of elements in responses
     # 2- get number of questions in a quizz
     # 3- compare both results
-    # 4- if same result then continue 
+    # 4- if same result then continue
     # 5- if not, redirect to quizz
     # =================
 
@@ -48,42 +50,31 @@ def result(request, pk):
 
     # Get responses into dict_values type
     values_resp = submitted_choices.values()
-    print(f"VALUES: {values_resp} // TYPE : {type(values_resp)}")
+
+    # Check if user passes quizz
     for element in values_resp:
         # Get first element of each values_resp list
         answer_string = element[0]
-        print(f"ELEMENT: {element} // {type(element)}")  # answer-1 etc
-        # if it contains this, add 1 to counter
         if answer_string.__contains__("CORRECT: True"):
-            print("oui")
             correct_answers += 1
-    print(f"COUNTER:::: {correct_answers}")
 
-    # TODO
-    # 1- trouver nombre de questions dans le quizz
-    # DELETE: quizz = Quizz.objects.get(pk=quizz_pk)
+    # Calculate result
     questions_quantity = Question.objects.filter(quizz=pk).count()
-    print(f"QUIZZ: {questions_quantity}")
-    # 2- Calculer le resultat en fonction du nb de questions ds quizz
-    result = int((correct_answers / questions_quantity)*100)
-    print(f"RESULT : {result}")
-    # 3- si res >= 50 alors c gagné
-    if result >= 50:
-        print("GAGNE")
-    # 4- sinon c perdu
+    result = int((correct_answers / questions_quantity) * 100)
+    quizz_title = Quizz.objects.get(pk=pk).title
+
+    if result >= 50:  # If 50% user passes
+        result_message = "Bravo! Vous avez validé ce quizz."
     else:
-        print("GAME OVER")
-    # ================
+        result_message = (
+            "Aïe, vous n'avez pas validé ce quizz. Une autre fois peut-être."
+        )
 
-    return render(request, "quizz/result.html", context={'pk': pk})
+    context = {
+        "pk": pk,
+        "result": result,
+        "result_message": result_message,
+        "quizz_title": quizz_title,
+    }
 
-
-""" TO DELETE
-    # Get values of the dict
-    # print(f"GET VALUES SUBMITTED: {submitted_choices.values()}")
-    # print(f"TYPE VALUES SUBMITTED: {type(submitted_choices.values())}")
-
-    # data = {}
-    # data['csrf_token'] = submitted_choices.get('csrfmiddlewaretoken')
-    # print(f"data['csrf_token']: {data['csrf_token']}")
-"""
+    return render(request, "quizz/result.html", context=context)
