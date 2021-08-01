c620a852c0a13e887178d3d377f6601efb423e7b
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-08-01 19:51:42 +0200

    🌱 feat/result: Start result page

--- quizz/urls.py
+++ quizz/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("home/", views.HomeView.as_view(), name="home"),
     path("quizz_list/", views.QuizzListView.as_view(), name="quizz_list"),
     path("quizz/<int:pk>/", views.QuizzDetailView.as_view(), name="quizz"),
+    path("quizz/result/<int:pk>/", views.result, name="result"),
 ]
