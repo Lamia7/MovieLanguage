c919ffb7bbabebab4ed160c6c2595df3d63c0efe
Lamia EL RALIMI <elralimi.lamia@gmail.com>
2021-09-05 18:44:29 +0200

    🧪 test: Add test on result page

--- tests/quizz/test_views.py
+++ tests/quizz/test_views.py
@@ -20,3 +20,18 @@ class TestQuizzView(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "quizz/quizz_list.html")
+
+    def test_result(self):
+        # checks the result page
+        pk = 2
+        data = {
+            "pk": 2,
+            "result": 66,
+            "result_message": "Bravo! Vous avez validé ce quizz.",
+            "quizz_title": "Quizz numéro 2"
+        }
+        response = self.client.post(
+            reverse("quizz:result", kwargs={'pk': pk}), data
+        )
+
+        self.assertEqual(response.status_code, 302)
